5523a884f197102c8e685dcbea4603fc12d45fa4
Alan Shaw <alan.shaw@protocol.ai>
2024-09-13 11:48:57 +0200

    fix: build

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link"
-import { Web3StorageLogo } from "./brand"
+import { Logo } from "./brand"
 
 export function NavLink ({href, children}: {href:string, children: React.ReactNode}) {
   return <Link href={href} className='inline-block uppercase font-medium text-lg md:text-xl py-4 md:py-2 px-2 mx-2 hover:text-zinc-100'>{children}</Link>
@@ -18,7 +18,7 @@ export function Header () {
     <header className='max-w-7xl mx-auto pt-4 md:pt-10 text-zinc-950'>
       <div className='md:flex items-center'>
         <div className='flex-1 px-3'>
-          <Web3StorageLogo>web3.storage</Web3StorageLogo>
+          <Logo>Storacha</Logo>
         </div>
         <nav className='hidden md:block bg-zinc-950/10 md:bg-transparent mt-4 md:mt-0 text-center md:text-left text-zinc-950'>
           <NavLink href='/products/web3storage'>Features</NavLink>
